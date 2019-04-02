6d5380e8bc2c290f4824ae909f6d9ee706ce31b9
KrishenG <kdgganpat@gmail.com>
2019-04-03 00:56:49 +0200

    added logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import logoHead from './logoHead.png'
+import './Logo.css';
+
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} src={logoHead} alt="Logo"/></div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;

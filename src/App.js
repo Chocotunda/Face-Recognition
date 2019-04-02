640fab9c97bc235b1d7eef14140606fa0c004598
KrishenG <kdgganpat@gmail.com>
2019-04-03 00:36:23 +0200

    created nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Navigation from './components/Navigation/Navigation';
 class App extends Component {
   render() {
     return (

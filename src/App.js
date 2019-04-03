a443d1bb523d4c28e2b9c75b66fcdf14c000d780
KrishenG <kdgganpat@gmail.com>
2019-04-03 09:58:09 +0200

    start adding  functionality form

--- src/App.js
+++ src/App.js
@@ -16,6 +16,21 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+onSubmit = () => [
+  console.log('OUCH!')
+]
+
   render() {
     return (
       <div className="App">
@@ -23,7 +38,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
         {/* <FaceRecognition /> */}
       </div>
     );

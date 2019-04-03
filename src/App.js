0820502f94adb52f7487850f96357b2a59470973
KrishenG <kdgganpat@gmail.com>
2019-04-03 10:38:42 +0200

    testing collor api model

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '4856bb19a4054b42830a09e249307f5b'
+});
 
 const particlesOptions = {
   particles: {
@@ -27,9 +33,22 @@ class App extends Component {
     console.log(event.target.value);
   };
 
-onSubmit = () => [
-  console.log('OUCH!')
-]
+  onButtonSubmit = () => {
+    console.log('OUCH!');
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        'https://samples.clarifai.com/face-det.jpg'
+      )
+      .then(
+        function(response) {
+          console.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
 
   render() {
     return (
@@ -38,8 +57,11 @@ onSubmit = () => [
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognistion = () => {
+  return (
+    <div className="center">
+      <img src="https://samples.clarifai.com/face-det.jpg" alt="" />
+    </div>
+  );
+};
+
+export default FaceRecognistion;

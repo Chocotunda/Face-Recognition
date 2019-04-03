1c128736694e51ad0dde89b44adec875b179396e
KrishenG <kdgganpat@gmail.com>
2019-04-03 12:58:54 +0200

    Face recognition feature works

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognistion = ({ imageUrl }) => {
+const FaceRecognistion = ({ imageUrl, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={imageUrl} alt="" width="500px" height="auto" />
+        <img
+          id="inputImage"
+          src={imageUrl}
+          alt=""
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        />
       </div>
     </div>
   );

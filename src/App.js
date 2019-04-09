1ba137f08598ab8f7d0b7e78e7fad62b253b30c4
Chocotunda <kdgganpat@gmail.com>
2019-04-09 19:56:16 +0200

    added catch error in fetch request

--- src/App.js
+++ src/App.js
@@ -23,23 +23,25 @@ const particlesOptions = {
   }
 };
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+};
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    };
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -53,12 +55,6 @@ class App extends Component {
       }
     });
   };
-
-  componentDidMount() {
-    fetch('http://localhost:3000')
-      .then((response) => response.json())
-      .then(console.log);
-  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace =
@@ -98,7 +94,8 @@ class App extends Component {
             .then((response) => response.json())
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
@@ -107,7 +104,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({ isSignedIn: true });
     }
